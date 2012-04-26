a9230b1933872837bb2754e33697fb412fb76cea
doxtop <andrii.zadorozhnii@gmail.com>
2012-04-27 02:41:10 +0300

    output error and transfer logs mod_esi cd /home/doxtop/prj/sync-server/apps/simple_sync/src

--- apps/simple_sync/src/simple_sync.erl
+++ apps/simple_sync/src/simple_sync.erl
@@ -1,6 +1,21 @@
 -module(simple_sync).
--export([test/3, service/3]).
+-export([test/3, log/3]).
 -include_lib("xmerl/include/xmerl.hrl").
+
+log(SessionID, Env, _Input) ->
+    mod_esi:deliver(SessionID, ["Content-Type:text/html\r\n\r\n" | log(Env)]).
+
+log([])->
+    "<ul><li>?error - for error log</li>
+    <li>?transfer - for transfer log</li><ul>";
+log([{query_string, "error"} | _Env])->
+    {ok, Log} = file:read_file("log/error.log"),
+    re:replace(erlang:binary_to_list(Log), "\n", "<br/>", [global, {return, list}]);
+log([{query_string, "transfer"} | _Env]) ->
+    {ok, Log} = file:read_file("log/transfer.log"),
+    re:replace(erlang:binary_to_list(Log), "\n", "<br/>", [global, {return, list}]);
+log([{_Key, _Value} | Env]) ->
+    log(Env).
 
 test(SessionID, Env, _Input) ->
     mod_esi:deliver(SessionID, ["Content-Type: text/html\r\n\r\n" | format(Env)]).
@@ -9,8 +24,3 @@ format([]) ->
     "";
 format([{Key, Value} | Env]) ->
     [io_lib:format("<b>~p:</b> ~p<br />\~n", [Key, Value]) | format(Env)].
-
-service(SessionID, _Env, _Input) ->
-     mod_esi:deliver(SessionID, ["Content-Type: text/html\r\n\r\n", "Service."]).
-
-
