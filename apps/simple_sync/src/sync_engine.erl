219d15a912058c5f8673fef530def10e218cd0eb
doxtop <andrii.zadorozhnii@gmail.com>
2012-05-30 03:21:21 +0300

    test data modification

--- apps/simple_sync/src/sync_engine.erl
+++ apps/simple_sync/src/sync_engine.erl
@@ -21,16 +21,27 @@ build_message({msg, Msg}, From, State)->
     {next_state, build_message, State#state{from=From}}.
 build_message({sync_cmd, E}, State)->
     error_logger:info_msg("build_message, receive 'sync' command", []),
+    build_command(E),
     {next_state, build_message, State};
 build_message({put_cmd, E}, State)->
     error_logger:info_msg("build_message, receive 'put' command", []),
     {next_state, build_message, State};
+build_message({final, E}, State)->
+    error_logger:info_msg("build_message, Final is the reason to terminate", []),
+    {stop, normal, State};
 build_message({add_cmd, E}, State)->
     error_logger:info_msg("build_message, receive 'add' command", []),
     {next_state, build_message, State};
-build_message({final, E}, State)->
-    error_logger:info_msg("build_message, Final is the reason to terminate", []),
-    {stop, normal, State}.
+build_message({replace_cmd, E}, State)->
+    error_logger:info_msg("build_message, receive 'replace' command", []),
+    {next_state, build_message, State}.
+
+build_command(E=#xmlElement{})->
+    %% - Target and Source -
+    % A relative URI can be specified, if the proper absolute URI can be constructed.
+    % from prefixing the respective Target or Source value from the SyncHdr to this relative URI.
+    % MUST use LocURI element. CGI script parameters can be appended to the URI to perform selection filtering on the server target.
+    error_logger:info_msg("bulding command...", [E#xmlElement.content]).
 
 handle_event(_Event, StateName, State)->
     error_logger:info_msg("handle_event", [StateName]),
@@ -49,11 +60,14 @@ code_change(_OldVsn, StateName, State, _Extra)->
     {ok, StateName, State}.
 
 template(E=#xmlElement{parents=[{'SyncBody',_}|_], name='Sync'})->
-    gen_fsm:send_event(self(), {sync_cmd, E});
+    gen_fsm:send_event(self(), {sync_cmd, E}),
+    xmerl_xs:xslapply(fun template/1, E);
 template(E=#xmlElement{parents=[{'SyncBody',_}|_], name='Put'})->
     gen_fsm:send_event(self(), {put_cmd, E});
 template(E=#xmlElement{parents=[{'Sync',_}|_], name='Add'})->
     gen_fsm:send_event(self(), {add_cmd, E});
+template(E=#xmlElement{parents=[{'Sync',_}|_], name='Replace'})->
+    gen_fsm:send_event(self(), {replace_cmd, E});
 template(E=#xmlElement{parents=[{'SyncBody',_}|_], name='Final'})->
     gen_fsm:send_event(self(), {final, E});
 template(E)->
